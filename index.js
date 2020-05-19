5e68c43a3cff8601de7e6223fd2191f9331a75a9
bhamann-collab <brockohamann@gmail.com>
2020-05-19 22:52:09 +0930

    Add employee functionality is now working

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function main() {
             message: 'What would you like to do?',
             choices: [
                 "View all employees",
-                "View all roles",
+                "Add employee",
                 "Update employee roles",
                 'Exit'
             ]
@@ -48,17 +48,73 @@ async function main() {
         let answer = Object.values(myCommand)[0]
 
         if(answer === 'Exit') {
-            console.log('it works')
+            console.log('bye')
             process.exit()
         }
 
         else if(answer === 'View all employees') {
             const items = await query(`
             USE employee_tracker_brock;
-            SELECT * from employee;
+            SELECT employee.first_name, employee.last_name, role.title, departments.name
+            FROM employee
+            INNER JOIN role ON employee.role_id = role.id
+            INNER JOIN departments ON role.department_id = departments.id;
+            
         `);
-        console.log(items)
+        console.log("first_name last_name title department")
+        console.log("_____________________________________")
+        items[1].map(arr => {
+            console.log(arr.first_name, arr.last_name, arr.title, arr.name)
+            })
+        }
+        else if(answer === 'Add employee') {
+            let newEmployee = await addUser()
+            console.log(newEmployee)
+
+            //Getting the id of the role
+            const roleID = await query(`
+            USE employee_tracker_brock;
+            SELECT id from role WHERE title="${newEmployee.employee}"
+            
+        `);
+        console.log(newEmployee.first_name)
+        console.log(roleID[1][0].id)
+
+            //Adding entry to database
+            await query(`
+            USE employee_tracker_brock;
+            ALTER TABLE employee AUTO_INCREMENT = 1;
+            INSERT INTO employee (first_name, last_name, role_id)
+            VALUES ('${newEmployee.first_name}', '${newEmployee.last_name}', '${roleID[1][0].id}');
+            `);
         }
     }
+}
+
+async function addUser() {
+    let addUserItems = await query(`
+    USE employee_tracker_brock;
+    SELECT title from role;
+`);
+    addUserItems = addUserItems[1].map(arr => arr.title)
+    return new Promise(resolve => {
+        inquirer
+        .prompt([
+            {
+                name: 'first_name',
+                message: 'Please enter first name'
+            },
+            {
+                name: 'last_name',
+                message: 'Please enter last name'
+            },
+            {
+                type: 'list',
+                name: 'employee',
+                message: 'Please choose the following role',
+                choices: addUserItems
+            }
+        ]).then(answers => resolve(answers))
+    })
 }
 main()
